85e939cc8e1b6e58d6e1d4f7b9c441dce8896ef3
Irtisah <164093007+Irtisah@users.noreply.github.com>
2024-03-19 23:13:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,53 +1,14 @@
 /********************************************************************************
-*  WEB322 – Assignment 02
+*  WEB322 – Assignment 03
 * 
 *  I declare that this assignment is my own work in accordance with Seneca's
 *  Academic Integrity Policy:
 * 
 *  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
 * 
-*  Name: _Irtisah Malik_____________________ Student ID: _154200224_____________ Date: _18 February 2024_____________
+*  Name: _Irtisah Malik_____________________ Student ID: _154200224_____________ Date: _19 March 2024_____________
 *
 ********************************************************************************/
-
-/*const legoData = require("./modules/legoSets");
-const express = require("express");
-
-
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.get("/", (req, res) => {
-  res.send("Assignment 2: Irtisah Malik - 154200224");
-});
-
-app.get("/lego/sets", (req, res) => {
-  legoData.getAllSets()
-    .then(allSets => res.json(allSets))
-    .catch(error => res.status(500).send(error.message));
-});
-
-app.get("/lego/sets/num-demo", (req, res) => {
-  const setNumber = "002-1";
-  legoData.getSetByNum(setNumber)
-    .then(setByNum => res.json(setByNum))
-    .catch(error => res.status(404).send(error.message));
-});
-
-app.get("/lego/sets/theme-demo", (req, res) => {
-  const theme = "tech";
-  legoData.getSetsByTheme(theme)
-    .then(setsByTheme => res.json(setsByTheme))
-    .catch(error => res.status(404).send(error.message));
-});
-
-legoData.initialize()
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
-  })
-  .catch(error => console.error("Initialization cannot be completed:", error.message));
-
-module.exports = app;*/
 
 const legoData = require("./modules/legoSets");
 const express = require("express");
